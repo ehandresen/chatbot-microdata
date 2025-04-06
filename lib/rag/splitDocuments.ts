1834ccdc7dfda97783253fb6c89aa27e359c564f
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2025-04-06 22:16:50 +0200

    Legger til splitDocuments-funksjon for å dele opp PDF-dokumentet i mindre tekstbiter med overlap

---
+++ lib/rag/splitDocuments.ts
@@ -0,0 +1,30 @@
+import { Document } from "langchain/document";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+
+import { loadPdf } from "./loadPdf";
+
+export async function splitDocuments(
+  rawDocuments: Document[]
+): Promise<Document[]> {
+  console.log("Splitting documents...");
+  // Eksperimenter med forskjellige chunkSize og overlapp, kan påvirke ytelsen til appen
+  const splitter = RecursiveCharacterTextSplitter.fromLanguage("markdown", {
+    chunkSize: 500,
+    chunkOverlap: 100,
+  });
+
+  const documentChunks = await splitter.splitDocuments(rawDocuments);
+
+  console.log(
+    `${rawDocuments.length} documents split into ${documentChunks.length} chunks.`
+  );
+
+  return documentChunks;
+}
+
+// Test
+async function main() {
+  const rawDocuments = await loadPdf();
+  await splitDocuments(rawDocuments);
+}
+main();
